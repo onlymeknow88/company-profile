56050ac44a20242a1459e6c9c2bf069de435e72b
Fadjri Wivindi <fwivindi@ptadaro.com>
2023-01-03 09:04:45 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    var navbar = $('.navbar');
+    $(window).scroll(function(){
+      if($(window).scrollTop() <=40){
+        navbar.removeClass('navbar-scroll');
+      }else{
+        navbar.addClass('navbar-scroll');
+      }
+    });
+  });
